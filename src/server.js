26fc5455cd54c92794cdb0c2805bb56dfb7074dd
abhinawagoo <abhinawago@gmail.com>
2025-04-07 22:17:55 +0530

    updating

--- src/server.js
+++ src/server.js
@@ -91,7 +91,3 @@ mongoose
       console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-    process.exit(1);
-  });
