a6c35e8ddb2c3bf4bfe2da9cff96add2fe87727c
abhinawagoo <abhinawago@gmail.com>
2025-05-28 13:32:55 +0530

    updating with sub domain in cors code

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use(morgan("dev")); // Logging
 const allowedOrigins = [
   "https://restaurant-feedback-frontend.vercel.app",
   "https://hoshloop.com",
-
+  "https://survey.hoshloop.com",
   "http://localhost:3000"
 ];
 
